60eb7b1818a5f291d96db07bed285d4209d02d9c
zhaobing <CreamBing1994@gmail.com>
2017-01-25 10:17:07 +0800

    [f][kafka][windows kafka bat脚本][先启动zookeeper后根据配置文件启动kafka][bing]

---
+++ src/main/resources/bat/start-zookeeper-properties.bat
@@ -0,0 +1,6 @@
+@echo off
+rem start F:\java\component\zookeeper-3.4.9\zookeeper-3.4.9\bin\zkServer.cmd
+start F:\java\component\zookeeper-3.4.9\zookeeper-3.4.9\bin\zkServer.cmd
+rem start kafka-server-start.bat F:\java\component\kafka_2.11-0.10.1.1\kafka_2.11-0.10.1.1\config\server.properties
+call F:\java\component\kafka_2.11-0.10.1.1\kafka_2.11-0.10.1.1\bin\windows\kafka-server-start.bat F:\java\component\kafka_2.11-0.10.1.1\kafka_2.11-0.10.1.1\config\server.properties
+
